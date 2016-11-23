400d5f5426b9b4cd56712eb1be998ab3de0c0d08
Brad Chao <brad@brad.tw>
2016-11-23 09:59:51 +0800

    v6

--- add2.php
+++ add2.php
@@ -9,11 +9,17 @@
         $stmt->bindParam(2, $newPass);
         if ($stmt->execute()){
             echo 'OK';
+            // 以下進行檔案上傳處理
+            $newid = $pdo->lastInsertId();
+            $upload = $_FILES['upload'];    // 上傳檔案的資訊 => 陣列
+            if ($upload['error'] == 0){
+                if (copy($upload['tmp_name'], "upload/icon_{$newid}.jpg")){
+                    echo 'OK2';
+                }
+            }
         }else{
             echo 'XX';
         }
-
-
     }catch (Exception $e){
         die("Server Busy");
     }
